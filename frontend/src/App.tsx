57766256c50ffd56e92b5cbbc7da2f975a11f35e
g_rocket <georgiiromanishko@gmail.com>
2025-03-20 12:23:30 +0100

    init project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import {useState} from 'react';
+import './App.css';
+import {Greet} from "../wailsjs/go/main/App";
+
+function App() {
+    const [resultText, setResultText] = useState("Please enter your name below 👇");
+    const [name, setName] = useState('');
+    const updateName = (e: any) => setName(e.target.value);
+    const updateResultText = (result: string) => setResultText(result);
+
+    function greet() {
+        Greet(name).then(updateResultText);
+    }
+
+    return (
+        <div className="w-dvw h-dvh">
+            <h1 className="w-full text-center">Hello app!!</h1>
+        </div>
+    )
+}
+
+export default App
