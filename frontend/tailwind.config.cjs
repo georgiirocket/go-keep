2843fa9f52f5d31ce279e1018f95013986f389fd
g_rocket <georgiiromanishko@gmail.com>
2025-03-22 12:44:19 +0100

    feat: hero ui

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,10 +1,25 @@
+const { heroui } = require('@heroui/react')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
+    screens: {
+      xs: '320px',
+      sm: '400px',
+      md: '744px',
+      lg: '1000px',
+      xl: '1440px',
+      '2xl': '1920px',
+    },
+    colors: {
+      mainColor: '#ff6c38',
+    },
   },
-  plugins: [],
+  darkMode: 'class',
+  plugins: [heroui()],
 }
